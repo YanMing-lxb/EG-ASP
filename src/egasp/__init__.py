97f6891e442f4e0909f78ec20b1c78dcd343f905
YanMing-CX <lxb-yanming@foxmail.com>
2025-05-06 10:29:16 +0800

    调整结构

--- src/egasp/__init__.py
+++ src/egasp/__init__.py
@@ -4,10 +4,16 @@
 '''
 
 import sys
-from .__main__ import get_egasp
+from .egasp_core import EG_ASP_Core
+
+# 实例化核心类
+eg = EG_ASP_Core()
+
+# 将 get_egasp 方法暴露为模块级别的函数
+get_egasp = eg.get_egasp  # 修改点：直接暴露 get_egasp 函数
 
 if sys.version_info[0] == 3:
     from .__main__ import main  # 显式导出 main() 供 CLI 入口使用
 else:
     # Don't import anything.
-    pass 
+    pass
